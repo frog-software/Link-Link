761d88acca6bdd9e4c0235f69ed0e8ed4960da01
Frog Dar <1270610465@qq.com>
2020-06-29 16:47:05 +0800

    更新注释

--- LIanLianKan/Map-Pic.cpp
+++ LIanLianKan/Map-Pic.cpp
@@ -1,8 +1,23 @@
+/*****************************************************************//**
+ * \file   Map-Pic.cpp
+ * \brief  Map类和Pic类的定义文件
+ * Map类和Pic类中各个成员叔叔的实现
+ * \author FrogDar
+ * \date   June 2020
+ *********************************************************************/
+
 #include "Map-Pic.h"
 #include "Control.h"
 extern Control* now;
-
-Pic::Pic(int _kind,int _x,int _y) :x{ _x }, y{ _y }, isVisible{ true }, isStroke{ true },kind{ _kind }
+/**
+ * .Pic类的构造函数
+ * 传入图标类型与坐标信息
+ * 默认可见、默认无边框
+ * \param _kind 当前图标的类型 （0表示障碍
+ * \param _x 在地图中的第几行
+ * \param _y 在地图中的第几列
+ */
+Pic::Pic(int _kind,int _x,int _y) :x{ _x }, y{ _y }, isVisible{ true }, isStroke{ false },kind{ _kind }
 {
 }
 
@@ -51,6 +66,10 @@ void Pic::setIsStroke(bool isStroke)
     this->isStroke = isStroke;
 }
 
+/**
+ * 绘画图标.
+ * 正常来说是绘画一张图片，这里是临时写法输出数字
+ */
 void Pic::draw()
 {
     char* s = new char[100];
@@ -58,15 +77,31 @@ void Pic::draw()
     now->xyprintf(x * 40, y * 30, s, 20);
 }
 
+/**
+ * 重载小于运算符.
+ * 为了可以排序
+ * \param b 与之相比较的图标
+ * \return 按坐标大小排序
+ */
 bool Pic::operator<(const Pic& b)
 {
     if (this->x != b.x)return this->x < b.x;
     else return this->y < b.y;
 }
 
-
+/**
+ * 地图类的构造函数
+ * 生成一个m行n列的地图
+ * \param _m  地图行数
+ * \param _n  地图列数
+ */
 Map::Map(int _m, int _n):m{_m},n{_n}
 {
+    /*
+    每种图形需要成对出现
+    所以利用正数变负数，负数变新的正数的方法
+    实现成对出现
+    */
     int t{ 0 };
     for (int i = 1; i <= m; i++)
         for (int j = 1; j <= n; j++) {
@@ -77,13 +112,24 @@ Map::Map(int _m, int _n):m{_m},n{_n}
         }
     RandomOrder();
 }
-
+/**
+ * 随机排列
+ *
+ * 重新排列所有Pic的位置并生成新的MatchedList *
+ */
 void Map::RandomOrder()
 {
     /*to be continued*/
     now->xyprintf(0, 0, "随机排列顺序的函数还没有写！", 20);
 }
 
+/**
+ * .判断是否能够连接
+ * 通过Matchedlist判断两个图标是否能够“连连看”
+ * \param a 图标1
+ * \param b 图标2
+ * \return  是否能够匹配
+ */
 bool Map::isMatch(Pic* a, Pic* b)
 {
     auto p{ std::pair<Pic*,Pic*>{a,b} };
@@ -92,12 +138,20 @@ bool Map::isMatch(Pic* a, Pic* b)
     return false;
 }
 
-
+/**
+ * 判断地图中是否还能进行匹配.
+ * 通过判断matchedlist是否为空就可以知道地图中是否还能匹配
+ * \return 能否匹配
+ */
 bool Map::anyMatch()
 {
     return !matchedlist.empty();
 }
 
+/**
+ * 绘画地图.
+ * 绘画地图中的每一个图标
+ */
 void Map::draw()
 {
     for (auto p : map)p->draw();
--- LIanLianKan/Map-Pic.h
+++ LIanLianKan/Map-Pic.h
@@ -1,10 +1,32 @@
 #pragma once
 #include<vector>
 #include<list>
+/**
+ * 图标类.
+ * 存储每一个小图标的信息
+ */
 class Pic{
 private:
-    int x, y, kind;
-    bool isVisible, isStroke;
+    /**
+     * 图标在地图中的坐标.
+     */
+    int x, y;
+    /**
+     * 图标的类型.
+     * 0为障碍物
+     * 其他数字对应某种可以消除的方块
+     */
+    int kind;
+    /**
+     * 是否可见.
+     * 一对图标被消除后要设置为不可见
+     */
+    bool isVisible;
+    /**
+     * 外边是否有边框.
+     * 被选中时，需要开启边框以示他人
+     */
+    bool isStroke;
 public:
     Pic(int _kind, int _x, int _y);
 
@@ -30,13 +52,29 @@ public:
 const int N = 100;
 class Map{
 private:
+    /**
+     * 地图大小.
+     */
     int m, n;
+    /**
+     * 存储地图中的图标.
+     * 利用指针的方式
+     */
     std::vector<Pic*>map;
+    /**
+     * 存储地图中可以匹配的序列.
+     * 利用list+pair
+     */
     std::list<std::pair<Pic*, Pic*> >matchedlist;
 public:
+    /*构建m*n的地图*/
     Map(int _m, int _n);
+    /*重新排序*/
     void RandomOrder();
+    /*是否“连连看”*/
     bool isMatch(Pic* a, Pic* b);
+    /*地图中是否能够连*/
     bool anyMatch();
+    /*绘制地图*/
     void draw();
 };
