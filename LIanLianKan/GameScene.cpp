69ed6b8454f728b27b3a2096f1af3d34b29c6f9a
Frog Dar <1270610465@qq.com>
2020-07-01 09:33:54 +0800

    更新注释

--- LIanLianKan/GameScene.cpp
+++ LIanLianKan/GameScene.cpp
@@ -4,27 +4,49 @@
 #include "SetScene.h"
 extern Control* now;
 
+/**
+ * @brief Construct a new Game Scene:: Game Scene object
+ * 生成一个m行n列的地图
+ * @param m 地图x方向图标个数
+ * @param n 地图y方向图标个数
+ */
 GameScene::GameScene(int m, int n)
 {
 	map = new Map{ m,n };
-		map->setConnectLine(nullptr);
+
+	map->setConnectLine(nullptr);
 }
 
+/**
+ * @brief Destroy the Game Scene:: Game Scene object
+ * 
+ */
 GameScene::~GameScene()
 {
 	delete map;
 }
 
+/**
+ * @brief 更新画面
+ * 按顺序从底层到顶层逐一更新画面
+ */
 void GameScene::update()
 {
+	/*加载背景图*/
 	now->putImage("./Pic/Game.png", 0, 0, 960, 640);
 
-	if (map->anyMatch() == false)map->RandomOrder();
+	/*更新图标信息*/
+	if (map->anyMatch() == false)
+		map->RandomOrder();
 	map->draw();
 
+	/*如果当前存在ConnectLine对象*/
 	if (map->getConnectLine() != nullptr) {
+		/*进行绘制*/
 		map->getConnectLine()->drawLine(now);
+		/*“倒计时”减一，还需要画多少帧该对象*/
 		map->getConnectLine()->cnt--;
+		/*“倒计时”结束之后，这个对象可以释放掉*/
 		if(map->getConnectLine()->cnt==0){
 			map->setConnectLine(nullptr);
 		}
@@ -32,6 +54,12 @@ void GameScene::update()
 
 }
 
+/**
+ * @brief 判定鼠标操作
+ * 
+ * @param x 鼠标点击的x
+ * @param y 鼠标点击的y
+ */
 void GameScene::onMouse(Sint32 x, Sint32 y)
 {
 	/*对于图标的判断*/
@@ -39,20 +67,24 @@ void GameScene::onMouse(Sint32 x, Sint32 y)
 	if (linearMousePositionOnMap >= 0 && map->map[linearMousePositionOnMap]->getValid())
 	{
 		if(last==nullptr){
+			/*这是第一次按图标，则图标加框*/
 			last = map->map[linearMousePositionOnMap];
 			last->setIsStroke(true);
 		}
 		else if(map->isMatch(last, map->map[linearMousePositionOnMap])==false){
+			/*如果无法匹配，则切换加框的图标*/
 			last->setIsStroke(false);
 			last = map->map[linearMousePositionOnMap];
 			last->setIsStroke(true);
 		}else{
+			/*如果完成匹配，那么要清除last（其他操作在匹配函数中已完成*/
 			last = nullptr;
 		}
 	//	printf("%d %d\n", map->map[linearMousePositionOnMap]->getX(), map->map[linearMousePositionOnMap]->getY());
 	}
 
 	/*功能按键的判断*/
+
 	/*设置界面*/
 	if (x >= 900 && x <= 960 && y >= 0 && y <= 50) new SetScene(this);
 }
