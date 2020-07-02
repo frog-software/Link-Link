c32667a547aeaa1e266f71f6eee0664cfc44ce34
QQK <67569413+KaseSerendipia@users.noreply.github.com>
2020-07-02 11:19:17 +0800

    增加了选择界面

---
+++ LIanLianKan/SelectScene.cpp
@@ -0,0 +1,143 @@
+#include "SelectScene.h"
+#include "Control.h"
+#include "GameScene.h"
+static int bgnow = 0;
+extern Control* now;
+
+SelectScene::SelectScene(Scene* last_) :last{ last_ }
+{
+	now->scene = this;
+}
+
+void SelectScene::update() {
+	/*选关界面背景*/
+	bgnow++;
+	if (bgnow == 49)bgnow = 0;
+	char* s = new char[50];
+	sprintf_s(s, 50, "./Pic/bgvideo/bgvideo%02d.png", bgnow);
+	now->putImage(s, 0, 0, 960, 640);
+	delete[] s;
+	now->putImage("./Pic/chooseTitle.png", 290, 20, 387, 111);
+	now->putImage("./Pic/level/level1_.png", 96, 150, 192, 128);
+	now->putImage("./Pic/level/level1.png", 96, 280, 254, 74);
+	now->putImage("./Pic/level/level2_.png", 384, 150, 192, 128);
+	now->putImage("./Pic/level/level3_.png", 672, 150, 192, 128);
+	now->putImage("./Pic/level/level4_.png", 96, 380, 192, 128);
+	now->putImage("./Pic/level/level5_.png", 384, 380, 192, 128);
+	now->putImage("./Pic/level/level6_.png", 672, 380, 192, 128);
+	if (1 == select)now->putImage("./Pic/level/level_stroke.png", 90, 140, 210, 220);
+	if (2 == select)now->putImage("./Pic/level/level_stroke.png", 378, 140, 210, 220);
+	if (3 == select)now->putImage("./Pic/level/level_stroke.png", 666, 140, 210, 220);
+	if (4 == select)now->putImage("./Pic/level/level_stroke.png", 90, 370, 210, 220);
+	if (5 == select)now->putImage("./Pic/level/level_stroke.png", 378, 370, 210, 220);
+	if (6 == select)now->putImage("./Pic/level/level_stroke.png", 666, 370, 210, 220);
+	if (1 == level) {
+		now->putImage("./Pic/level/lock.png", 420, 150, 120, 120);
+		now->putImage("./Pic/level/lock.png", 708, 150, 120, 120);
+		now->putImage("./Pic/level/lock.png", 132, 380, 120, 120);
+		now->putImage("./Pic/level/lock.png", 420, 380, 120, 120);
+		now->putImage("./Pic/level/lock.png", 708, 380, 120, 120);
+		now->putImage("./Pic/level/locked.png", 384, 280, 254, 74);
+		now->putImage("./Pic/level/locked.png", 672, 280, 254, 74);
+		now->putImage("./Pic/level/locked.png", 96, 510, 254, 74);
+		now->putImage("./Pic/level/locked.png", 384, 510, 254, 74);
+		now->putImage("./Pic/level/locked.png", 672, 510, 254, 74);
+	}
+	if (2 == level) {
+		now->putImage("./Pic/level/lock.png", 708, 150, 120, 120);
+		now->putImage("./Pic/level/lock.png", 132, 380, 120, 120);
+		now->putImage("./Pic/level/lock.png", 420, 380, 120, 120);
+		now->putImage("./Pic/level/lock.png", 708, 380, 120, 120);
+		now->putImage("./Pic/level/level2.png", 384, 280, 254, 74);
+		now->putImage("./Pic/level/locked.png", 672, 280, 254, 74);
+		now->putImage("./Pic/level/locked.png", 96, 510, 254, 74);
+		now->putImage("./Pic/level/locked.png", 384, 510, 254, 74);
+		now->putImage("./Pic/level/locked.png", 672, 510, 254, 74);
+	}
+	if (3 == level) {
+		now->putImage("./Pic/level/lock.png", 132, 380, 120, 120);
+		now->putImage("./Pic/level/lock.png", 420, 380, 120, 120);
+		now->putImage("./Pic/level/lock.png", 708, 380, 120, 120);
+		now->putImage("./Pic/level/level2.png", 384, 280, 254, 74);
+		now->putImage("./Pic/level/level3.png", 672, 280, 254, 74);
+		now->putImage("./Pic/level/locked.png", 96, 510, 254, 74);
+		now->putImage("./Pic/level/locked.png", 384, 510, 254, 74);
+		now->putImage("./Pic/level/locked.png", 672, 510, 254, 74);
+	}
+	if (4 == level) {
+		now->putImage("./Pic/level/lock.png", 420, 380, 120, 120);
+		now->putImage("./Pic/level/lock.png", 708, 380, 120, 120);
+		now->putImage("./Pic/level/level2.png", 384, 280, 254, 74);
+		now->putImage("./Pic/level/level3.png", 672, 280, 254, 74);
+		now->putImage("./Pic/level/level4.png", 96, 510, 254, 74);
+		now->putImage("./Pic/level/locked.png", 384, 510, 254, 74);
+		now->putImage("./Pic/level/locked.png", 672, 510, 254, 74);
+	}
+	if (5 == level) {
+		now->putImage("./Pic/level/lock.png", 708, 380, 120, 120);
+		now->putImage("./Pic/level/level2.png", 384, 280, 254, 74);
+		now->putImage("./Pic/level/level3.png", 672, 280, 254, 74);
+		now->putImage("./Pic/level/level4.png", 96, 510, 254, 74);
+		now->putImage("./Pic/level/level5.png", 384, 510, 254, 74);
+		now->putImage("./Pic/level/locked.png", 672, 510, 254, 74);
+	}
+	if (6 == level) {
+		now->putImage("./Pic/level/lock.png", 708, 380, 120, 120);
+		now->putImage("./Pic/level/level2.png", 384, 280, 254, 74);
+		now->putImage("./Pic/level/level3.png", 672, 280, 254, 74);
+		now->putImage("./Pic/level/level4.png", 96, 510, 254, 74);
+		now->putImage("./Pic/level/level5.png", 384, 510, 254, 74);
+		now->putImage("./Pic/level/level6.png", 672, 510, 254, 74);
+	}
+}
+
+void SelectScene::onMouse(Sint32 x, Sint32 y) {
+	if (x >= 96 && x <= 288 && y >= 150 && y <= 280) {
+		now->scene = new GameScene(last, 6, 5, 1, false);
+		delete this;
+	}
+	if (x >= 384 && x <= 576 && y >= 150 && y <= 280 && level >= 2) {
+		now->scene = new GameScene(last, 8, 6, 2, false);
+		delete this;
+	}
+	if (x >= 672 && x <= 864 && y >= 150 && y <= 280 && level >= 3) {
+		now->scene = new GameScene(last, 10, 7, 3, false);
+		delete this;
+	}
+	if (x >= 96 && x <= 288 && y >= 380 && y <= 510 && level >= 4) {
+		now->scene = new GameScene(last, 12, 8, 4, false);
+		delete this;
+	}
+	if (x >= 384 && x <= 576 && y >= 380 && y <= 510 && level >= 5) {
+		now->scene = new GameScene(last, 14, 9, 5, false);
+		delete this;
+	}
+	if (x >= 672 && x <= 864 && y >= 380 && y <= 510 && level >= 6) {
+		now->scene = new GameScene(last, 16, 11, 6, true);
+		delete this;
+	}
+}
+
+void SelectScene::onMouseMotion(Sint32 x, Sint32 y) {
+	if (x >= 96 && x <= 288 && y >= 150 && y <= 280) {
+		select = 1;
+	}
+	else if (x >= 384 && x <= 576 && y >= 150 && y <= 280) {
+		select = 2;
+	}
+	else if (x >= 672 && x <= 864 && y >= 150 && y <= 280) {
+		select = 3;
+	}
+	else if (x >= 96 && x <= 288 && y >= 380 && y <= 510) {
+		select = 4;
+	}
+	else if (x >= 384 && x <= 576 && y >= 380 && y <= 510) {
+		select = 5;
+	}
+	else if (x >= 672 && x <= 864 && y >= 380 && y <= 510) {
+		select = 6;
+	}
+	else {
+		select = 0;
+	}
+}
---
+++ LIanLianKan/SelectScene.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Control.h"
+
+class SelectScene :public Scene {
+private:
+	Scene* last = nullptr;
+	int select = 0;
+public:
+	int level = 1;
+	SelectScene(Scene* last_);
+	~SelectScene() = default;
+	void update()override;
+	void onMouse(Sint32 x, Sint32 y) override;
+	void onMouseMotion(Sint32 x, Sint32 y) override;
+};
